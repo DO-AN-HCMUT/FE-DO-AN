f043aa5516e00807da70525d3c7da1439b03e4c1
Anh Dinh <dlna85.acc@gmail.com>
2024-05-13 13:12:47 +0700

    feat: homepage

---
+++ components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import Button from '../Button';
+
+export default function Header() {
+  return (
+    <div className='fixed flex w-full items-center justify-between bg-[#3c3c3c] p-4'>
+      <Image src='/images/logo.png' alt='logo' width={40} height={40} className='' />
+      <div className='flex'>
+        <Button className='me-4' onClick={() => {}}>
+          + New Project
+        </Button>
+        <Image src='/images/header/bell.png' alt='notification' width={40} height={40} className='me-2' />
+        <Image src='/images/header/avatar.jpeg' alt='profile' width={40} height={40} className='rounded-full' />
+      </div>
+    </div>
+  );
+}
---
+++ components/SideBar/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+
+export default function SideBar() {
+  return (
+    <div className='h-full w-[15%] border-r-[1px] border-[#ccc] px-2 py-4'>
+      <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+        <Image src='/images/sideBar/home.png' alt='home' width={20} height={20} className='me-3' />
+        <p>Home</p>
+      </div>
+      <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+        <Image src='/images/sideBar/dashboard.png' alt='dashboard' width={20} height={20} className='me-3' />
+        <p>Dashboard</p>
+      </div>
+      <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+        <Image src='/images/sideBar/task.png' alt='task' width={20} height={20} className='me-3' />
+        <p>Task</p>
+      </div>
+      <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+        <Image src='/images/sideBar/timeline.png' alt='timeline' width={20} height={20} className='me-3' />
+        <p>Timeline</p>
+      </div>
+      <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+        <Image src='/images/sideBar/message.png' alt='message' width={20} height={20} className='me-3' />
+        <p>Messages</p>
+      </div>
+    </div>
+  );
+}
