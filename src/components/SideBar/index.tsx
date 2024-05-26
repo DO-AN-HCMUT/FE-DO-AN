b32a95e27495be8877b94f7f0508a4b607c5b265
vwaizer <lightwing2208@gmail.com>
2024-05-26 19:11:47 +0700

    feat: add modal in new project

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function SideBar() {
   return (
-    <div className='h-full w-[15%] border-r-[1px] border-[#ccc] px-2 py-4'>
+    <div className='h-full w-[15%] cursor-pointer border-r-[1px] border-[#ccc] px-2 py-4'>
       <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
         <Image src='/images/sideBar/home.png' alt='home' width={20} height={20} className='me-3' />
         <p>Home</p>
