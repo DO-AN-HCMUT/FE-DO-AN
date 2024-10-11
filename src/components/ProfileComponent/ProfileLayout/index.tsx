e8d597eb7b0d85857605c200450923f856beff37
vwaizer <lightwing2208@gmail.com>
2024-10-11 18:14:39 +0700

    refactor: complete logic JS

--- src/components/ProfileComponent/ProfileLayout/index.tsx
+++ src/components/ProfileComponent/ProfileLayout/index.tsx
@@ -11,25 +11,54 @@ export default function ProfileLayout() {
   const [fullName, setFullName] = useState<string>('fullName');
   const [email, setEmail] = useState<string>('Email');
   const [birthday, setBirthday] = useState<string>(new Date().toString());
-  const [file, setFile] = useState<string>('https://avatar.iran.liara.run/public');
   const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [img, setImg] = useState<any>('https://avatar.iran.liara.run/public');
   const getProfile = async () => {
     try {
       const profile = await api.get('/user/me');
       setFullName(profile.data.payload.fullName);
       setEmail(profile.data.payload.email);
       setBirthday(profile.data.payload.birthday);
+      if (profile.data.payload?.avatar.length > 0) {
+        setImg(profile.data.payload.avatar);
+      }
     } catch (error) {
       // console.log(error);
     }
   };
 
   const inputFile = useRef(null);
-
+  const uploadImg = async () => {
+    if (typeof img !== 'string') {
+      try {
+        const formData = new FormData();
+        formData.append('file', img);
+        await api.post('/user/uploadImg', formData);
+      } catch (error) {
+        // console.log(error);
+      }
+    }
+  };
+  const updateProfile = async () => {
+    try {
+      const userData = {
+        fullName,
+        email,
+        birthday,
+      };
+      await api.put('/user/update', userData);
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+  const handleSave = () => {
+    uploadImg();
+    updateProfile();
+    setIsEdit(false);
+  };
   const handleChange = (e: any) => {
-    // console.log(e.target.files[0]);
-    const url = URL.createObjectURL(e.target.files[0]);
-    setFile(url);
+    const imgLink = e.target.files[0];
+    setImg(imgLink);
   };
   const handleClick = () => {
     setIsEdit(true);
@@ -44,13 +73,14 @@ export default function ProfileLayout() {
         <Typography variant='h2'>Profile</Typography>
       </div>
       <div className=' flex min-h-10 flex-row'>
-        <div className='w-fit bg-yellow-500 p-2'>
+        <div className='w-fit  p-2 pt-0'>
           <Image
-            src={file}
-            alt='logo'
+            src={typeof img == 'string' ? img : URL.createObjectURL(img)}
+            alt='avatar'
             width={500}
             height={200}
             style={{ maxHeight: '200px', maxWidth: '100%', borderRadius: '8px' }}
+            className='bg-yellow-500'
           />
           <Button variant='contained' className='w-full' onClick={() => handleClick()}>
             Upload
@@ -70,7 +100,7 @@ export default function ProfileLayout() {
         </div>
       </div>
       <div className=' w-full pt-1'>
-        <Button variant='contained' color='success' disabled={!isEdit} className='w-full'>
+        <Button variant='contained' color='success' disabled={!isEdit} className='w-full' onClick={() => handleSave()}>
           Save
         </Button>
       </div>
