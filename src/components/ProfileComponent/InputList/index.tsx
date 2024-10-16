23501adc0dd5cecb6804b27fd9c41704d058b83e
Quang Vinh <113232837+vwaizer@users.noreply.github.com>
2024-10-16 14:21:23 +0700

    feat: profile page (#18)

---
+++ src/components/ProfileComponent/InputList/index.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable no-tabs */
+
+import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import { TextField } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+
+type ProfileProps = {
+  fullName: string;
+  email: string;
+  birthday: string;
+  setFullName: any;
+  setEmail: any;
+  setBirthday: any;
+  setIsEdit: any;
+};
+export default function InputList(props: ProfileProps) {
+  const { fullName, email, birthday, setFullName, setEmail, setBirthday, setIsEdit } = props;
+  const handleChange = (value: any, method: any) => {
+    if (value) {
+      setIsEdit(true);
+      method(value);
+    }
+  };
+  return (
+    <div className='w-full'>
+      <div className='flex flex-row  p-1'>
+        <PersonOutlinedIcon fontSize='large' />
+        <TextField
+          id='fullName'
+          label='Full Name'
+          value={fullName}
+          fullWidth
+          onChange={(e) => handleChange(e.target.value, setFullName)}
+        />
+      </div>
+      <div className='flex w-full  flex-col justify-between lg:flex-row'>
+        <div className='flex w-6/12 flex-row  p-1'>
+          <DateRangeIcon fontSize='large' />
+          <div>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label='Birthday'
+                value={dayjs(birthday)}
+                onChange={(value) => handleChange(value?.toDate().toString(), setBirthday)}
+                disableFuture
+                views={['year', 'month', 'day']}
+              />
+            </LocalizationProvider>
+          </div>
+        </div>
+        <div className='flex w-full flex-row  p-1'>
+          <AlternateEmailOutlinedIcon fontSize='large' />
+          <TextField
+            id='email'
+            label='Email'
+            value={email}
+            fullWidth
+            onChange={(e) => handleChange(e.target.value, setEmail)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/ProfileComponent/ProfileLayout/index.tsx
@@ -0,0 +1,114 @@
+import { Button, Typography } from '@mui/material';
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+
+import api from '@/services/api';
+
+import InputList from '../InputList';
+import ProjectList from '../ProjectList';
+
+/* eslint-disable no-tabs */
+export default function ProfileLayout() {
+  const [fullName, setFullName] = useState<string>('fullName');
+  const [email, setEmail] = useState<string>('Email');
+  const [birthday, setBirthday] = useState<string>(new Date().toString());
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [img, setImg] = useState<any>('https://avatar.iran.liara.run/public');
+  const getProfile = async () => {
+    try {
+      const profile = await api.get('/user/me');
+      setFullName(profile.data.payload.fullName);
+      setEmail(profile.data.payload.email);
+      setBirthday(profile.data.payload.birthday);
+      if (profile.data.payload?.avatar.length > 0) {
+        setImg(profile.data.payload.avatar);
+      }
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+
+  const inputFile = useRef(null);
+  const uploadImg = async () => {
+    if (typeof img !== 'string') {
+      try {
+        const formData = new FormData();
+        formData.append('file', img);
+        await api.post('/user/uploadImg', formData);
+      } catch (error) {
+        // console.log(error);
+      }
+    }
+  };
+  const updateProfile = async () => {
+    try {
+      const userData = {
+        fullName,
+        email,
+        birthday,
+      };
+      await api.put('/user/update', userData);
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+  const handleSave = () => {
+    uploadImg();
+    updateProfile();
+    setIsEdit(false);
+  };
+  const handleChange = (e: any) => {
+    const imgLink = e.target.files[0];
+    setImg(imgLink);
+  };
+  const handleClick = () => {
+    inputFile?.current?.click();
+    setIsEdit(true);
+  };
+  useEffect(() => {
+    getProfile();
+  }, []);
+  return (
+    <div className='w-full'>
+      <div>
+        <Typography variant='h2'>Profile</Typography>
+      </div>
+      <div className=' flex min-h-10 flex-row'>
+        <div className='w-fit p-2 pt-0'>
+          <div>
+            <Image
+              src={typeof img == 'string' ? img : URL.createObjectURL(img)}
+              alt='avatar'
+              width={500}
+              height={200}
+              style={{ height: '150px' }}
+              className='max-w-full rounded-full border-2 border-solid'
+            />
+          </div>
+
+          <Button variant='contained' className='w-full' onClick={() => handleClick()}>
+            Upload
+          </Button>
+          <input ref={inputFile} type='file' style={{ display: 'none' }} accept='image/*' onChange={handleChange} />
+        </div>
+        <div className=' w-full'>
+          <InputList
+            fullName={fullName}
+            email={email}
+            birthday={birthday}
+            setFullName={setFullName}
+            setEmail={setEmail}
+            setBirthday={setBirthday}
+            setIsEdit={setIsEdit}
+          />
+        </div>
+      </div>
+      <ProjectList />
+      <div className='mt-2 w-full'>
+        <Button variant='contained' color='success' disabled={!isEdit} className='w-full' onClick={() => handleSave()}>
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+}
