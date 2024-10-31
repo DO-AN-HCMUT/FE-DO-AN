4b0da7c588c5ffec79b8bc052677864c766edd8a
vwaizer <lightwing2208@gmail.com>
2024-10-31 10:04:57 +0700

    feat: call api and fix side bar

--- src/components/CalendarComponent/CalendarGrid/index.tsx
+++ src/components/CalendarComponent/CalendarGrid/index.tsx
@@ -4,6 +4,11 @@
 
 import { DayPilot, DayPilotCalendar } from '@daypilot/daypilot-lite-react';
 import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+import api from '@/services/api';
+// import { start } from 'repl';
+// import { text } from 'stream/consumers';
 
 const colors = [
   { name: 'Green', id: '#6aa84f' },
@@ -16,17 +21,17 @@ const colors = [
   { name: 'Light Red', id: '#ff0000' },
   { name: 'Purple', id: '#af8ee5' },
 ];
-const styles = {
-  wrap: {
-    display: 'flex',
-  },
-  left: {
-    marginRight: '10px',
-  },
-  main: {
-    flexGrow: '1',
-  },
-};
+// const styles = {
+//   wrap: {
+//     display: 'flex',
+//   },
+//   left: {
+//     marginRight: '10px',
+//   },
+//   main: {
+//     flexGrow: '1',
+//   },
+// };
 export default function CalendarGrid() {
   const oldParticipants = [
     { name: '1', id: 1 },
@@ -54,57 +59,57 @@ export default function CalendarGrid() {
     calendar?.events.update(e);
   };
 
-  const contextMenu = new DayPilot.Menu({
-    items: [
-      {
-        text: 'Delete',
-        onClick: async (args) => {
-          calendar?.events.remove(args.source);
-        },
-      },
-      {
-        text: '-',
-      },
-      {
-        text: 'Edit...',
-        onClick: async (args) => {
-          await editEvent(args.source);
-        },
-      },
-    ],
-  });
+  // const contextMenu = new DayPilot.Menu({
+  //   items: [
+  //     {
+  //       text: 'Delete',
+  //       onClick: async (args) => {
+  //         calendar?.events.remove(args.source);
+  //       },
+  //     },
+  //     {
+  //       text: '-',
+  //     },
+  //     {
+  //       text: 'Edit...',
+  //       onClick: async (args) => {
+  //         await editEvent(args.source);
+  //       },
+  //     },
+  //   ],
+  // });
 
-  const onBeforeEventRender = (args: DayPilot.CalendarBeforeEventRenderArgs) => {
-    args.data.areas = [
-      {
-        top: 5,
-        right: 5,
-        width: 20,
-        height: 20,
-        symbol: 'icons/daypilot.svg#minichevron-down-2',
-        fontColor: '#fff',
-        backColor: '#00000033',
-        style: 'border-radius: 25%; cursor: pointer;',
-        toolTip: 'Show context menu',
-        action: 'ContextMenu',
-      },
-    ];
+  // const onBeforeEventRender = (args: DayPilot.CalendarBeforeEventRenderArgs) => {
+  //   args.data.areas = [
+  //     {
+  //       top: 5,
+  //       right: 5,
+  //       width: 20,
+  //       height: 20,
+  //       symbol: 'icons/daypilot.svg#minichevron-down-2',
+  //       fontColor: '#fff',
+  //       backColor: '#00000033',
+  //       style: 'border-radius: 25%; cursor: pointer;',
+  //       toolTip: 'Show context menu',
+  //       action: 'ContextMenu',
+  //     },
+  //   ];
 
-    const participants = args.data.tags?.participants || 0;
-    if (participants > 0) {
-      args.data.areas.push({
-        bottom: 5,
-        left: 5,
-        width: 24,
-        height: 24,
-        action: 'None',
-        backColor: '#00000033',
-        fontColor: '#fff',
-        text: participants,
-        style: 'border-radius: 50%; border: 2px solid #fff; font-size: 18px; text-align: center;',
-      });
-    }
-  };
+  //   const participants = args.data.tags?.participants || 0;
+  //   if (participants > 0) {
+  //     args.data.areas.push({
+  //       bottom: 5,
+  //       left: 5,
+  //       width: 24,
+  //       height: 24,
+  //       action: 'None',
+  //       backColor: '#00000033',
+  //       fontColor: '#fff',
+  //       text: participants,
+  //       style: 'border-radius: 50%; border: 2px solid #fff; font-size: 18px; text-align: center;',
+  //     });
+  //   }
+  // };
 
   const initialConfig: DayPilot.CalendarConfig = {
     viewType: 'Week',
@@ -112,79 +117,96 @@ export default function CalendarGrid() {
   };
 
   const [config, setConfig] = useState(initialConfig);
+  const getTask = async () => {
+    try {
+      const result = await api.get('/task/getAll');
+      if (!calendar || calendar?.disposed()) {
+        return;
+      }
+      const tasksEvent = result.data.payload.map((item: any) => {
+        return {
+          id: item.code,
+          text: item.title,
+          start: new Date(item.createdDate).toString(),
+          end: new Date(item.endDate).toString(),
+          backColor: colors[Math.floor(Math.random() * 10)].id,
+        };
+      });
+      // const events: DayPilot.EventData[] = [
+      //   {
+      //     id: 1,
+      //     text: 'Event 1',
+      //     start: '2024-10-02T10:30:00',
+      //     end: '2024-10-02T13:00:00',
+      //     tags: {
+      //       participants: 2,
+      //     },
+      //   },
+      //   {
+      //     id: 2,
+      //     text: 'Event 2',
+      //     start: '2024-10-02T09:30:00',
+      //     end: '2024-10-05T11:30:00',
+      //     backColor: '#6aa84f',
+      //     tags: {
+      //       participants: 1,
+      //     },
+      //   },
+      //   {
+      //     id: 3,
+      //     text: 'Event 3',
+      //     start: '2024-10-03T12:00:00',
+      //     end: '2024-10-03T15:00:00',
+      //     backColor: '#f1c232',
+      //     tags: {
+      //       participants: 3,
+      //     },
+      //   },
+      //   {
+      //     id: 4,
+      //     text: 'Event 4',
+      //     start: '2024-10-01T11:30:00',
+      //     end: '2024-10-01T14:30:00',
+      //     backColor: '#cc4125',
+      //     tags: {
+      //       participants: 2,
+      //     },
+      //   },
+      // ];
+      const events: DayPilot.EventData[] = tasksEvent;
 
-  useEffect(() => {
-    if (!calendar || calendar?.disposed()) {
-      return;
+      const startDate = new Date().toISOString();
+
+      calendar.update({ startDate, events });
+    } catch (error: any) {
+      toast.error(typeof error?.response?.data == 'object' ? error?.response?.data.message : error?.message);
     }
-    const events: DayPilot.EventData[] = [
-      {
-        id: 1,
-        text: 'Event 1',
-        start: '2024-10-02T10:30:00',
-        end: '2024-10-02T13:00:00',
-        tags: {
-          participants: 2,
-        },
-      },
-      {
-        id: 2,
-        text: 'Event 2',
-        start: '2024-10-02T09:30:00',
-        end: '2024-10-05T11:30:00',
-        backColor: '#6aa84f',
-        tags: {
-          participants: 1,
-        },
-      },
-      {
-        id: 3,
-        text: 'Event 3',
-        start: '2024-10-03T12:00:00',
-        end: '2024-10-03T15:00:00',
-        backColor: '#f1c232',
-        tags: {
-          participants: 3,
-        },
-      },
-      {
-        id: 4,
-        text: 'Event 4',
-        start: '2024-10-01T11:30:00',
-        end: '2024-10-01T14:30:00',
-        backColor: '#cc4125',
-        tags: {
-          participants: 2,
-        },
-      },
-    ];
-
-    const startDate = '2024-10-01';
-
-    calendar.update({ startDate, events });
+  };
+  useEffect(() => {
+    getTask();
   }, [calendar]);
 
-  const onTimeRangeSelected = async (args: DayPilot.CalendarTimeRangeSelectedArgs) => {
-    const modal = await DayPilot.Modal.prompt('Create a new event:', 'Event 1');
-    calendar?.clearSelection();
-    if (modal.canceled) {
-      return;
-    }
-    // console.log('modal.result', modal.result, calendar);
-    calendar?.events.add({
-      start: args.start,
-      end: args.end,
-      id: DayPilot.guid(),
-      text: modal.result,
-      tags: {
-        participants: 1,
-      },
-    });
-  };
+  // const onTimeRangeSelected = async (args: DayPilot.CalendarTimeRangeSelectedArgs) => {
+  //   const modal = await DayPilot.Modal.prompt('Create a new event:', 'Event 1');
+  //   calendar?.clearSelection();
+  //   if (modal.canceled) {
+  //     return;
+  //   }
+  //   // console.log('modal.result', modal.result, calendar);
+  //   calendar?.events.add({
+  //     start: args.start,
+  //     end: args.end,
+  //     id: DayPilot.guid(),
+  //     text: modal.result,
+  //     tags: {
+  //       participants: 1,
+  //     },
+  //   });
+  // };
 
   return (
     <div>
-      <DayPilotCalendar
+      {/* <DayPilotCalendar
         {...config}
         onTimeRangeSelected={onTimeRangeSelected}
         onEventClick={async (args) => {
@@ -193,7 +215,8 @@ export default function CalendarGrid() {
         contextMenu={contextMenu}
         onBeforeEventRender={onBeforeEventRender}
         controlRef={setCalendar}
-      />
+      /> */}
+      <DayPilotCalendar {...config} controlRef={setCalendar} />
     </div>
   );
 }
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -16,10 +16,10 @@ export default function SideBar() {
         <Image src='/images/sideBar/task.png' alt='task' width={20} height={20} className='me-3' />
         <p>Task</p>
       </div>
-      <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+      <Link className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white' href='/calendar'>
         <Image src='/images/sideBar/timeline.png' alt='timeline' width={20} height={20} className='me-3' />
         <p>Timeline</p>
-      </div>
+      </Link>
       <Link className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white' href='/chat'>
         <Image src='/images/sideBar/message.png' alt='message' width={20} height={20} className='me-3' />
         <p>Messages</p>
