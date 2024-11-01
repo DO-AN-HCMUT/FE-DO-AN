6bfaa2c40d9f9cce50b34fa85c433c2a092e6901
Quang Vinh <113232837+vwaizer@users.noreply.github.com>
2024-11-01 10:45:36 +0700

    feat: calendar page (#28)

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -6,33 +6,42 @@ export default function SideBar() {
   const isMdOrSmaller = useMediaQuery('(max-width:960px)');
   return (
     <div className='h-full w-[15%] cursor-pointer border-r-[1px] border-[#ccc] px-2 py-4 '>
-      <Tooltip title='home' disableHoverListener={!isMdOrSmaller}>
-        <Link className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white' href='/'>
+      <Tooltip title='Home' disableHoverListener={!isMdOrSmaller}>
+        <Link
+          className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'
+          href='/'
+        >
           <Image src='/images/sideBar/home.png' alt='home' width={20} height={20} className='me-3' />
           <p className='hidden md:inline'>Home</p>
         </Link>
       </Tooltip>
-      <Tooltip title='Dashboard'>
-        <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+      <Tooltip title='Dashboard' disableHoverListener={!isMdOrSmaller}>
+        <div className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'>
           <Image src='/images/sideBar/dashboard.png' alt='dashboard' width={20} height={20} className='me-3' />
           <p className='hidden md:inline'>Dashboard</p>
         </div>
       </Tooltip>
-      <Tooltip title='Task'>
-        <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+      <Tooltip title='Task' disableHoverListener={!isMdOrSmaller}>
+        <div className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'>
           <Image src='/images/sideBar/task.png' alt='task' width={20} height={20} className='me-3' />
           <p className='hidden md:inline'>Task</p>
         </div>
       </Tooltip>
-      <Tooltip title='Timeline'>
-        <div className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white'>
+      <Tooltip title='Timeline' disableHoverListener={!isMdOrSmaller}>
+        <Link
+          className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'
+          href='/calendar'
+        >
           <Image src='/images/sideBar/timeline.png' alt='timeline' width={20} height={20} className='me-3' />
           <p className='hidden md:inline'>Timeline</p>
-        </div>
+        </Link>
       </Tooltip>
 
-      <Tooltip title='Messages'>
-        <Link className='mb-3 flex items-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white' href='/chat'>
+      <Tooltip title='Messages' disableHoverListener={!isMdOrSmaller}>
+        <Link
+          className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'
+          href='/chat'
+        >
           <Image src='/images/sideBar/message.png' alt='message' width={20} height={20} className='me-3' />
           <p className='hidden md:inline'>Messages</p>
         </Link>
