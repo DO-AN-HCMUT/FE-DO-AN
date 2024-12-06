1b8e91efab4929e4c520618ff881d6ac497ebeed
vwaizer <lightwing2208@gmail.com>
2024-12-06 11:00:55 +0700

    fix: side bar and add project

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -15,18 +15,12 @@ export default function SideBar() {
           <p className='hidden md:inline'>Home</p>
         </Link>
       </Tooltip>
-      <Tooltip title='Dashboard' disableHoverListener={!isMdOrSmaller}>
+      {/* <Tooltip title='Dashboard' disableHoverListener={!isMdOrSmaller}>
         <div className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'>
           <Image src='/images/sideBar/dashboard.png' alt='dashboard' width={20} height={20} className='me-3' />
           <p className='hidden md:inline'>Dashboard</p>
         </div>
-      </Tooltip>
-      <Tooltip title='Task' disableHoverListener={!isMdOrSmaller}>
-        <div className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'>
-          <Image src='/images/sideBar/task.png' alt='task' width={20} height={20} className='me-3' />
-          <p className='hidden md:inline'>Task</p>
-        </div>
-      </Tooltip>
+      </Tooltip> */}
       <Tooltip title='Timeline' disableHoverListener={!isMdOrSmaller}>
         <Link
           className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'
