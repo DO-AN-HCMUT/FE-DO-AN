834cfeacc63f8ef1a85d108ad0433861d1c3af11
Quang Vinh <113232837+vwaizer@users.noreply.github.com>
2024-12-06 11:00:04 +0700

    feat: side bar: project members (#41)

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,9 +1,14 @@
 import { Tooltip, useMediaQuery } from '@mui/material';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
 
 export default function SideBar() {
   const isMdOrSmaller = useMediaQuery('(max-width:960px)');
+  const searchParams = useSearchParams();
+
+  const projectId = searchParams.get('project-id')!;
+
   return (
     <div className='h-full w-[15%] cursor-pointer border-r-[1px] border-[#ccc] px-2 py-4 '>
       <Tooltip title='Home' disableHoverListener={!isMdOrSmaller}>
@@ -46,6 +51,17 @@ export default function SideBar() {
           <p className='hidden md:inline'>Projects</p>
         </Link>
       </Tooltip>
+      {projectId && (
+        <Tooltip title='Members' disableHoverListener={!isMdOrSmaller}>
+          <Link
+            className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'
+            href={`/projects/members?project-id=${projectId}`}
+          >
+            <Image src='/images/sideBar/members.png' alt='members' width={20} height={20} className='me-3' />
+            <p className='hidden md:inline'>Members</p>
+          </Link>
+        </Tooltip>
+      )}
       <Tooltip title='Messages' disableHoverListener={!isMdOrSmaller}>
         <Link
           className='mb-3 flex items-center justify-center rounded-lg px-4 py-2 hover:bg-primary hover:text-white md:justify-normal'
