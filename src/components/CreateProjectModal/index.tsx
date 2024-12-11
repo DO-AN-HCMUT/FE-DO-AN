c0e72f6dd3e7a25a23ef68a111cd71cfaa714d75
dlnanh85 <dlna85.acc@gmail.com>
2024-12-11 08:55:20 +0700

    fix: minor bugs

--- src/components/CreateProjectModal/index.tsx
+++ src/components/CreateProjectModal/index.tsx
@@ -89,45 +89,43 @@ export default function CreateProjectModal({ isAdding, onClose, onSubmitSuccess 
           Create Project
         </Typography>
         <div className='flex w-full flex-col items-stretch justify-between pt-5'>
-          <form onSubmit={handleSubmit}>
-            <div className='mb-4 flex grow flex-col justify-center'>
-              <p className='mb-2 ps-1 font-semibold'>Project Name</p>
-              <TextInput
-                placeholder='Enter project name'
-                value={projectName ?? ''}
-                onInput={(projectName) => {
-                  setProjectName(projectName);
-                  setDebouncedProjectName(projectName);
-                  setIsProjectKeyLoading(true);
-                }}
-                type='text'
-              />
-            </div>
-            <div className='mb-4 flex grow flex-col justify-center'>
-              {projectName && (
-                <p className='mb-2 ps-1 text-sm font-light'>
-                  Project Key: {isProjectKeyLoading ? <Spinner className='inline-block' /> : <span>{projectKey}</span>}
-                </p>
-              )}
-            </div>
-            <div className='mb-4'>
-              <p className='mb-2 ps-1 font-semibold'>Description</p>
-              <textarea
-                placeholder='Describe your project'
-                value={description}
-                onInput={(e) => setDescription(e.currentTarget.value)}
-                className='h-20 w-full rounded-lg border-[1px] border-[#616161] p-3 placeholder:text-[#616161] '
-              />
-            </div>
-            <div className='flex flex-row justify-between self-end'>
-              <Button className='me-4' onClick={onClose} type='neutral-positive'>
-                Cancel
-              </Button>
-              <Button isDisabled={!projectName} onClick={() => {}}>
-                Submit
-              </Button>
-            </div>
-          </form>
+          <div className='mb-4 flex grow flex-col justify-center'>
+            <p className='mb-2 ps-1 font-semibold'>Project Name</p>
+            <TextInput
+              placeholder='Enter project name'
+              value={projectName ?? ''}
+              onInput={(projectName) => {
+                setProjectName(projectName);
+                setDebouncedProjectName(projectName);
+                setIsProjectKeyLoading(true);
+              }}
+              type='text'
+            />
+          </div>
+          <div className='mb-4 flex grow flex-col justify-center'>
+            {projectName && (
+              <p className='mb-2 ps-1 text-sm font-light'>
+                Project Key: {isProjectKeyLoading ? <Spinner className='inline-block' /> : <span>{projectKey}</span>}
+              </p>
+            )}
+          </div>
+          <div className='mb-4'>
+            <p className='mb-2 ps-1 font-semibold'>Description</p>
+            <textarea
+              placeholder='Describe your project'
+              value={description}
+              onInput={(e) => setDescription(e.currentTarget.value)}
+              className='h-20 w-full rounded-lg border-[1px] border-[#616161] p-3 placeholder:text-[#616161] '
+            />
+          </div>
+          <div className='flex flex-row justify-between self-end'>
+            <Button className='me-4' onClick={onClose} type='neutral-positive'>
+              Cancel
+            </Button>
+            <Button isDisabled={!projectName} onClick={handleSubmit}>
+              Submit
+            </Button>
+          </div>
         </div>
       </Box>
     </Modal>
